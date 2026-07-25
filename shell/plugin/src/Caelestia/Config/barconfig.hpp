083b24e412b1c7156d3378959bafe6ddeb423210
ladybug-me <rajveer2027singh@gmail.com>
2026-07-26 01:08:12 +0530

    fix: centred the non-material you workspace switcher icon

--- shell/plugin/src/Caelestia/Config/barconfig.hpp
+++ shell/plugin/src/Caelestia/Config/barconfig.hpp
@@ -52,7 +52,7 @@ class BarWorkspaces : public ConfigObject {
     CONFIG_PROPERTY(bool, useIcon, true)
     CONFIG_PROPERTY(QString, label, u" "_s)
     CONFIG_PROPERTY(QString, occupiedLabel, u" 󰮯"_s)
-    CONFIG_PROPERTY(QString, activeLabel, u" 󰮯"_s)
+    CONFIG_PROPERTY(QString, activeLabel, u"󰮯 "_s)
     CONFIG_PROPERTY(QString, capitalisation, u"preserve"_s)
     CONFIG_GLOBAL_PROPERTY(QVariantList, specialWorkspaceIcons)
     CONFIG_GLOBAL_PROPERTY(QVariantList, windowIcons,
